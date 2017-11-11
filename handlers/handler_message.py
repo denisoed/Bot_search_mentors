c24dfe5fd379e1638b6b9485c2f4533bd6a422ed
denisoed <gufig93@mail.ru>
2017-11-11 14:41:42 +0300

    Add function for get user name

--- handlers/handler_message.py
+++ handlers/handler_message.py
@@ -2,9 +2,23 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from . import data
 from assets import messages
 
+
+# Necessary variables
+user_name = '' 
+
+# Start function
 def hello(bot, update):
     update.message.reply_text(messages.get_name)
-
-# Handler queries
+                       
+# Handler incoming messages
 def message_processing(bot, update):
-    print(update._effective_message.text)
+    if user_name == '':
+        get_user_name(update)
+        update.message.reply_text(text='Ну привет %s' % user_name)
+    else:
+        update.message.reply_text(text=messages.unknown_message)
+
+# Get user name
+def get_user_name(update):
+    global user_name
+    user_name = update._effective_message.text
